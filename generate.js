c7c9ec605bfcf373d30d34073fb4a1462098fb05
Joa_sss <quabotgithub@gmail.com>
2022-08-04 21:28:57 +0200

    Initial Commit

---
+++ generate.js
@@ -0,0 +1,47 @@
+// Characters to use for the password
+const baseCharacters = "abcdefghijklmnopqrstuvwxyz";
+const capitalCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// These are doubled to ensure the chances of getting these are equal.
+const numbersCharacters = "01234567890123456789";
+const symbolsCharacters = "!@#$%^&*!@#$%^&*!@#$%^&*";
+
+function getPassword (form) {
+
+    // Get the values from the form.
+    let amount = form.amountCharacters.value;
+    if (amount < 2) amount = 2;
+    if (amount > 100) amount = 100;
+
+    const capitalLetters = form.capitalLetters.checked;
+    const numbers = form.numbers.checked;
+    const symbols = form.symbols.checked;
+
+    let charSet = baseCharacters;
+
+    // Make the list of charcters to pick the character from.
+    if (capitalLetters) charSet = charSet + capitalCharacters;
+    if (numbers) charSet = charSet + numbersCharacters;
+    if (symbols) charSet = charSet + symbolsCharacters;
+    
+    let password = "";
+
+    // Generate the password.
+    for (let i = 0; i < amount; i++) {
+        const rndChar = charSet.charAt(Math.floor(Math.random() * charSet.length));
+        password = password + rndChar;
+    }
+
+    const element = document.getElementById("password");
+    element.innerHTML = password;
+}
+
+
+function loadTheme() {
+    let theme = window.localStorage.getItem('theme');
+    if (theme !== 'dark' && theme !== 'light') {window.localStorage.setItem('theme', 'light');}
+    if (theme === 'dark' || theme === 'light') {
+        document.body.classList.remove('theme-dark', 'theme-light');
+        document.body.classList.add(`theme-${theme}`);
+    }
+}
---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+const { shell } = require('electron');
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        minWidth: 250,
+        minHeight: 100,
+        width: 500,
+        height: 500,
+        webPreferences: {
+          nodeIntegration: true
+        },
+        title: "Password Generator",
+        autoHideMenuBar: true,
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    });
+});
+
+
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+});
+
+
