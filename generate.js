2c49695f8f5b436d4abd46985258c379cbd431eb
foxl <67546646+foxlldev@users.noreply.github.com>
2022-08-04 20:55:08 +0100

    Update generate.js

--- generate.js
+++ generate.js
@@ -39,9 +39,8 @@ function getPassword (form) {
 
 function loadTheme() {
     let theme = window.localStorage.getItem('theme');
-    if (theme !== 'dark' && theme !== 'light') {window.localStorage.setItem('theme', 'light');}
     if (theme === 'dark' || theme === 'light') {
         document.body.classList.remove('theme-dark', 'theme-light');
         document.body.classList.add(`theme-${theme}`);
-    }
+    } else { window.localStorage.setItem('theme', 'light'); }
 }
